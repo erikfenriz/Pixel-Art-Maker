42c722ff6bf9bfe8945be85d82f8a8c92b7876fe
Erik Rybalkin <erikfenriz@gmail.com>
2017-12-22 19:55:57 +0200

    error stable

--- designs.js
+++ designs.js
@@ -16,8 +16,11 @@ function makeGrid() {
     if (height > 50 || width > 50 || height < 1 || width < 1) {
         if (!error.classList.contains("error")) {
             error.classList.toggle("error");
-            error.innerText = "the dimension are. has to be smaller than 50 and bigger than 0"
+            error.innerText = "the dimension are has to be smaller than 50 and bigger than 0"
         }
+    }else{
+        error.innerText = "Draw ;)"
+        $("div").removeClass("error");
     }
 for (let x = 0; x < height; x++){
     $('#pixel_canvas').append(row);
