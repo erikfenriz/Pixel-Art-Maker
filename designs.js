76594d3aa7f537cc007a8e69e0848308a86b68fc
Erik Rybalkin <erikfenriz@gmail.com>
2017-12-22 19:27:41 +0200

    changed color palette

--- designs.js
+++ designs.js
@@ -2,18 +2,21 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let height, width, color, submit, table, d;
+let height, width, color, submit, table, d, row, col;
 
 function makeGrid() {
     d = document;
     height = $("#input_height").val();
     width = $("#input_width").val();
-    color =  $("#colorPicker").val();
+    color =  $(".jscolor").val();
     submit = $("#submit");
     table = $("#pixel_canvas");
+    row  = $("<tr></tr>");
+    col = $("<td></td>");
 for (let x = 0; x < height; x++){
+    $('#pixel_canvas').append(row);
     for(let y = 0;y < width; y++){
-
+        row.append(col);
     }
 }
 }
