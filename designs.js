41694c7e20a470319cd58f9f0cab48da78a45798
Erik Rybalkin <erikfenriz@gmail.com>
2017-12-22 18:14:10 +0200

    changed logo

--- designs.js
+++ designs.js
@@ -2,12 +2,19 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let height, width, color;
+let height, width, color, submit;
 
 function makeGrid() {
-    color =  $("#colorPicker").val();
     height = $("#input_height").val();
     width = $("#input_width").val();
+    color =  $("#colorPicker").val();
+    submit = $("#submit").val();
+    console.log(height,submit,color,width);
+for (let x = 0; x < height; x++){
+    for(let y = 0;y < width; y++){
+
+    }
+}
 }
 
 
