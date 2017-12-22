6228d07e41dcc7c904e068e52519584ddb968127
Erik Rybalkin <erikfenriz@gmail.com>
2017-12-22 17:21:54 +0200

    variables

--- designs.js
+++ designs.js
@@ -2,9 +2,15 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var height, width, color;
+
 
 function makeGrid() {
+    color =  $("#colorPicker").val();
+    height = $("#input_height").val();
+    width = $("#input_width").val();
+}
+
+
 
 // Your code goes here!
-
-}
