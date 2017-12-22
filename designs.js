4391d1e8f25b35cc440225e53807154ad84e1907
Erik Rybalkin <erikfenriz@gmail.com>
2017-12-22 18:28:23 +0200

    stable let

--- designs.js
+++ designs.js
@@ -2,21 +2,18 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let height, width, color, submit;
+let height, width, color, submit, table, d;
 
 function makeGrid() {
+    d = document;
     height = $("#input_height").val();
     width = $("#input_width").val();
     color =  $("#colorPicker").val();
-    submit = $("#submit").val();
-    console.log(height,submit,color,width);
+    submit = $("#submit");
+    table = $("#pixel_canvas");
 for (let x = 0; x < height; x++){
     for(let y = 0;y < width; y++){
 
     }
 }
 }
-
-
-
-// Your code goes here!
