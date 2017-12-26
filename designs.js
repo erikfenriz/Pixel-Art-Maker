305aa3739b236e01c1d6adb8322cb2f5255f283e
Erik Rybalkin <erikfenriz@gmail.com>
2017-12-26 04:25:27 +0200

    color picks properly, draws correctly, unused variable are terminated

--- designs.js
+++ designs.js
@@ -1,44 +1,42 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-let height, width, color, submit, table, d, startRow, endRow, col, reset;
-
-
+let height, width, color, reset;
 function makeGrid() {
     $("#pixel_canvas").html("");
-    d = document;
     height = $("#input_height").val();
     width = $("#input_width").val();
-    color =  $(".jscolor").val();
-    submit = $("#submit");
-    table = $("#pixel_canvas");
-    startRow  = '<tr>';
-    endRow = "</tr>";
-    col = $("<td></td>");
     if (height > 50 || width > 50 || height < 1 || width < 1) {
         if (!error.classList.contains("error")) {
             error.classList.toggle("error");
             error.innerText = "the dimension are has to be smaller than 50 and bigger than 0";
             backUp();
         }
-    }else{
+    } else {
         error.innerText = "";
         $("div").removeClass("error");
         for (let x = 0; x < height; x++) {
             $('#pixel_canvas').append('<tr></tr>');
         }
-        for(let y = 0;y < width; y++){
-            $('#pixel_canvas tr').each(function(){
+        for (let y = 0; y < width; y++) {
+            $('#pixel_canvas tr').each(function () {
                 $(this).append('<td></td>');
             });
         }
     }
-    }
-
-
+}
+color = $('#colorPicker');
+$(document).on("mousedown", "tr td", function () {
+    let colorValue = color.val();
+    $(this).css('background-color', colorValue);
+    $('tr td').bind("mousemove", function () {
+        let colorValue = color.val();
+        $(this).css('background-color', colorValue);
+    }).mouseup(function() {
+        $('td').unbind('mousemove');
+    });
+    $('tr td').on('dblclick',function () {
+        $(this).css('background-color', "#FFFFFF")
+    })
+});
 reset = $("#pixel_canvas").html();
-
-function backUp(){
+function backUp() {
     $("#pixel_canvas").html(reset);
 }
